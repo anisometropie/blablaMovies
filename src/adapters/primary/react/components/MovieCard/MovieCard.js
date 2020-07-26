22dde3e7a4273105c3a9aa4a209381ffd8c39f2a
Valentin Chanas <valentin.chanas@dcbrain.com>
2020-07-26 19:29:31 +0200

    highlight selected movies

--- src/adapters/primary/react/components/MovieCard/MovieCard.js
+++ src/adapters/primary/react/components/MovieCard/MovieCard.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 import { Dialog, Button } from '@material-ui/core'
 
+import { selectors as loggedUserSelectors } from 'store/reducers/loggedUser'
 import MovieDetails from '../MovieDetails'
 import Card from '../../base.ui/Card'
+import { includes } from 'lodash'
 
 const useStyles = makeStyles({
   image: {
     cursor: 'pointer'
   },
-  button: { color: 'white', alignSelf: 'center' }
+  button: { color: 'white', alignSelf: 'center' },
+  selectedMovie: { boxSizing: 'border-box', border: 'solid 2px yellow' }
 })
 
 function MovieCard({ movie, onVote }) {
+  const user = useSelector(loggedUserSelectors.getState)
   const [showDetails, setShowDetails] = useState(false)
   const classes = useStyles()
   const toggleDialog = state => () => setShowDetails(state)
@@ -20,7 +25,13 @@ function MovieCard({ movie, onVote }) {
   const { infos } = movie
   return (
     <React.Fragment>
-      <Card>
+      <Card
+        className={
+          user?.votes?.find(m => m.movieId === movie.id) !== undefined
+            ? classes.selectedMovie
+            : ''
+        }
+      >
         <img
           className={classes.image}
           onClick={onVote}
